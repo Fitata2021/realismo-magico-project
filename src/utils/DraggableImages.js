8144dca27a330fe1081b6f2bee653393f087ac0e
Cristian Fitata <cristianfitata@gmail.com>
2024-01-02 15:56:13 -0500

    settings-images-artworkcanvas

--- src/utils/DraggableImages.js
+++ src/utils/DraggableImages.js
@@ -1,12 +1,31 @@
-import img1 from "../images/canva-images/mariposa.png";
-import img2 from "../images/canva-images/pez.png";
-import img3 from "../images/canva-images/jaguar.png";
-import img4 from "../images/canva-images/armadura.png";
-import img5 from "../images/canva-images/flowers.png";
-import img6 from "../images/canva-images/hiena.png";
-import img7 from "../images/canva-images/hongos.png";
-import img8 from "../images/canva-images/lechuza.png";
-import img9 from "../images/canva-images/woman.png";
+// import img1 from "../images/canva-images/mariposa.png";
+// import img2 from "../images/canva-images/pez.png";
+// import img3 from "../images/canva-images/jaguar.png";
+// import img4 from "../images/canva-images/armadura.png";
+// import img5 from "../images/canva-images/flowers.png";
+// import img6 from "../images/canva-images/hiena.png";
+// import img7 from "../images/canva-images/hongos.png";
+// import img8 from "../images/canva-images/lechuza.png";
+// import img9 from "../images/canva-images/woman.png";
+
+const img1 =
+  "https://imagedelivery.net/W9-AoheGofN712tx-fnwKA/faf42829-d81c-43f6-79dc-6de1cc349d00/public";
+const img2 =
+  "https://imagedelivery.net/W9-AoheGofN712tx-fnwKA/f6b86436-63e8-45da-fb2e-0bf8b3f7dc00/public";
+const img3 =
+  "https://imagedelivery.net/W9-AoheGofN712tx-fnwKA/bd2fac63-b68e-489f-eacf-529332eccf00/public";
+const img4 =
+  "https://imagedelivery.net/W9-AoheGofN712tx-fnwKA/68d51205-55b7-4010-5845-b39207b4ae00/public";
+const img5 =
+  "https://imagedelivery.net/W9-AoheGofN712tx-fnwKA/1faf6489-adf0-48f0-a184-35175620f500/public";
+const img6 =
+  "https://imagedelivery.net/W9-AoheGofN712tx-fnwKA/da297958-60b5-48e0-ba2c-4c9baff5f400/public";
+const img7 =
+  "https://imagedelivery.net/W9-AoheGofN712tx-fnwKA/52559ba2-e423-4719-cdf7-4a4993853300/public";
+const img8 =
+  "https://imagedelivery.net/W9-AoheGofN712tx-fnwKA/c897d4a6-9d3b-40f8-e092-5c8b5de15600/public";
+const img9 =
+  "https://imagedelivery.net/W9-AoheGofN712tx-fnwKA/6cec8aa1-f0c2-4a52-86a0-82f18a493000/public";
 
 const draggableImages = [
   {
